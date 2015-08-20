886d1ab036f70a2734edcd2365ba17b1f31484ca
eben-roux <me@ebenroux.co.za>
2015-08-20 14:43:02 +0200

    - message distribution guide

---
+++ guides/Shuttle.Distribution/Shuttle.Distribution.Worker/Host.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Shuttle.Core.Host;
+using Shuttle.ESB.Core;
+
+namespace Shuttle.Distribution.Worker
+{
+	public class Host : IHost, IDisposable
+	{
+		private IServiceBus _bus;
+
+		public void Start()
+		{
+			_bus = ServiceBus.Create().Start();
+		}
+
+		public void Dispose()
+		{
+			_bus.Dispose();
+		}
+	}
+}
