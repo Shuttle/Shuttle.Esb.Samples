530e24ccfcbee9fc56dbfe3a4619c3fb3f5841fa
eben-roux <me@ebenroux.co.za>
2015-08-25 08:55:21 +0200

    - dependency injection guide

---
+++ guides/Shuttle.DependencyInjection/Shuttle.DependencyInjection.Server/Host.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Castle.MicroKernel.Registration;
+using Castle.Windsor;
+using Shuttle.Core.Host;
+using Shuttle.DependencyInjection.EMail;
+using Shuttle.ESB.Castle;
+using Shuttle.ESB.Core;
+
+namespace Shuttle.DependencyInjection.Server
+{
+	public class Host : IHost, IDisposable
+	{
+		private IServiceBus _bus;
+		private WindsorContainer _container;
+
+		public void Start()
+		{
+			_container = new WindsorContainer();
+
+			_container.Register(Component.For<IEMailService>().ImplementedBy<EMailService>());
+
+			// register all the message handler in this assembly
+			_container.Register(
+				Classes.FromThisAssembly()
+				.BasedOn(typeof(IMessageHandler<>))
+				.WithServiceFromInterface(typeof(IMessageHandler<>))
+				.LifestyleTransient()
+				);
+
+			_bus = ServiceBus.Create(
+				c => c.MessageHandlerFactory(new CastleMessageHandlerFactory(_container))
+				).Start();
+		}
+
+		public void Dispose()
+		{
+			_bus.Dispose();
+		}
+	}
+}
---
+++ guides/Shuttle.DependencyInjection/Shuttle.DependencyInjection.Server/RegisterMemberHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Shuttle.Core.Infrastructure;
+using Shuttle.DependencyInjection.EMail;
+using Shuttle.ESB.Core;
+using Shuttle.DependencyInjection.Messages;
+
+namespace Shuttle.DependencyInjection.Server
+{
+	public class RegisterMemberHandler : IMessageHandler<RegisterMemberCommand>
+	{
+		private readonly IEMailService _emailService;
+
+		public RegisterMemberHandler(IEMailService emailService)
+		{
+			Guard.AgainstNull(emailService, "emailService");
+
+			_emailService = emailService;
+		}
+
+		public void ProcessMessage(HandlerContext<RegisterMemberCommand> context)
+		{
+			Console.WriteLine();
+			Console.WriteLine("[MEMBER REGISTERED] : user name = '{0}'", context.Message.UserName);
+			Console.WriteLine();
+
+			_emailService.Send(context.Message.UserName);
+		}
+
+		public bool IsReusable
+		{
+			get { return true; }
+		}
+	}
+}
