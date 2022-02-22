dfbc00a06004deb86e57fa1c28ccc230f6e0f452
Eben <me@ebenroux.co.za>
2022-02-22 15:38:51 +0200

    - request/response minor refactoring

--- Shuttle.RequestResponse/Shuttle.RequestResponse.Client/Program.cs
+++ Shuttle.RequestResponse/Shuttle.RequestResponse.Client/Program.cs
@@ -15,7 +15,6 @@ namespace Shuttle.RequestResponse.Client
 			var container = new WindsorComponentContainer(new WindsorContainer());
 
 			container.Register<IAzureStorageConfiguration, DefaultAzureStorageConfiguration>();
-
 			container.RegisterServiceBus();
 
 			using (var bus = container.Resolve<IServiceBus>().Start())
