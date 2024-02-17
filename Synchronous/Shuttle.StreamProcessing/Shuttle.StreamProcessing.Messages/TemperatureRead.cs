01d7e990b6e93b0c09bc2f8b510819c9dd2fddae
Eben <me@ebenroux.co.za>
2024-02-17 08:15:25 +0200

    - sync stream

--- Synchronous/Shuttle.StreamProcessing/Shuttle.StreamProcessing.Messages/TemperatureRead.cs
+++ Synchronous/Shuttle.StreamProcessing/Shuttle.StreamProcessing.Messages/TemperatureRead.cs
@@ -2,7 +2,7 @@
 {
     public class TemperatureRead
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Minute { get; set; }
         public decimal Celsius { get; set; }
     }
